d66f7a1876b43741133df2ad9983698b8f883a42
crist <crist@CRISTIAN>
2025-10-17 15:41:25 -0500

    Agregar archivos de proyecto.

---
+++ Models/DbDevopsContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace TalentoLocal.Models;
+
+public partial class DbDevopsContext : DbContext
+{
+    public DbDevopsContext()
+    {
+    }
+
+    public DbDevopsContext(DbContextOptions<DbDevopsContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Postulacion> Postulaciones { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Server=tcp:s-devopsg1.database.windows.net,1433;Initial Catalog=db-devops;Persist Security Info=False;User ID=cagarcias@unimagdalena.edu.co;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Authentication=\"Active Directory Integrated\";");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Postulacion>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Postulac__3213E83FFAF8BFE7");
+
+            entity.ToTable("Postulaciones", "TalentoLocal");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.CreateTime)
+                .HasColumnType("datetime")
+                .HasColumnName("create_time");
+            entity.Property(e => e.Descripcion).HasColumnName("descripcion");
+            entity.Property(e => e.Titulo)
+                .HasMaxLength(255)
+                .HasColumnName("titulo");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/Postulacion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TalentoLocal.Models;
+
+public partial class Postulacion
+{
+    public int Id { get; set; }
+
+    public DateTime? CreateTime { get; set; }
+
+    public string? Titulo { get; set; }
+
+    public string? Descripcion { get; set; }
+}
