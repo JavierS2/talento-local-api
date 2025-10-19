5e822d35001e88cc7954842f795b6018a007828e
Carlos Romero <romero.ca20042018@gmail.com>
2025-10-18 20:55:54 -0500

    feat: sixth model (History)

---
+++ Models/History.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using TalentoLocal.Models.enums;
+namespace TalentoLocal.Models
+{
+    public class History
+    {
+        public int Id { get; set; }
+
+        public EventType EventType { get; set; }
+
+        public int ResponsibleUserId { get; set; }
+
+        public int ReferenceId { get; set; }
+
+        public string EventDescription { get; set; } = string.Empty;
+
+        public DateTime EventDate { get; set; } = DateTime.UtcNow;
+
+        public string PreviousState { get; set; } = string.Empty;
+
+        public string NewState { get; set; } = string.Empty;
+
+        public string Observations { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    }
+}
