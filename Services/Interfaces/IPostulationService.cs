9b0589d1c1727fa4e6d90e1b46f6a702b4260a1f
Carlos Romero <romero.ca20042018@gmail.com>
2025-11-18 00:31:14 -0500

    feat: add new functions in the services

--- Services/Interfaces/IPostulationService.cs
+++ Services/Interfaces/IPostulationService.cs
@@ -4,9 +4,9 @@ namespace TalentoLocal.Services.Interfaces
 {
     public interface IPostulationService
     {
-        Task<IEnumerable<PostulationDTO>> GetAllAsync();
-        Task<PostulationDTO> GetByIdAsync(int id);
-        Task<PostulationDTO> CreateAsync(PostulationDTO postulationDTO);
+        Task<IEnumerable<PostulationResponseDTO>> GetAllAsync();
+        Task<PostulationResponseDTO> GetByIdAsync(int id);
+        Task<PostulationResponseDTO> CreateAsync(PostulationDTO postulationDTO);
         Task<bool> UpdateAsync(int id, PostulationDTO postulationDTO);
         Task<bool> DeleteAsync(int id);
     }
