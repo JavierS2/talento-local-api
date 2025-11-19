ddb2cee0a075d6bdd71b5b93df272c7a5477e6e2
JavierS2 <javieandres2004@gmail.com>
2025-11-19 00:14:27 -0500

    feat: Add new model Favorite

---
+++ Migrations/20251119045546_FavoriteModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TalentoLocal.Migrations
+{
+    /// <inheritdoc />
+    public partial class FavoriteModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Favorites",
+                schema: "TalentoLocal",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    user_id = table.Column<int>(type: "int", nullable: false),
+                    offer_id = table.Column<int>(type: "int", nullable: false),
+                    create_at = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Favorites", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Favorites_Offers_offer_id",
+                        column: x => x.offer_id,
+                        principalSchema: "TalentoLocal",
+                        principalTable: "Offers",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Favorites_offer_id",
+                schema: "TalentoLocal",
+                table: "Favorites",
+                column: "offer_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Favorites_user_id_offer_id",
+                schema: "TalentoLocal",
+                table: "Favorites",
+                columns: new[] { "user_id", "offer_id" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Favorites",
+                schema: "TalentoLocal");
+        }
+    }
+}
---
+++ Models/Favorite.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TalentoLocal.Models
+{
+    [Table("Favorites")]
+    public class Favorite
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("user_id")]
+        public int UserId { get; set; }
+
+        [Required]
+        [Column("offer_id")]
+        public int OfferId { get; set; }
+
+        // FK hacia Offers
+        [ForeignKey("OfferId")]
+        public Offer? Offer { get; set; }
+
+        [Required]
+        [Column("create_at")]
+        public DateTime CreatedAt { get; set; }
+    }
+}
