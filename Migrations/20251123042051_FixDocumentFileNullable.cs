a94c7ee61a841a7bab085e0d0d5ddbdebc78db5e
Carlos Romero <romero.ca20042018@gmail.com>
2025-11-22 23:30:40 -0500

    feat: add new migrations

---
+++ Migrations/20251123042051_FixDocumentFileNullable.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TalentoLocal.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixDocumentFileNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "document_file",
+                schema: "TalentoLocal",
+                table: "Postulations",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "document_file",
+                schema: "TalentoLocal",
+                table: "Postulations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
